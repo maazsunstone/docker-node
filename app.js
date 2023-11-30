21357e30fe990ce3adfc3c89563ab4dc8d1d6eb6
maazsunstone <maaz.abdullah@sunstone.in>
2023-11-30 21:16:33 +0530

    api response changes

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ const app = express();
 app.get("/", (req, res) => {
   res.status(200).json({
     status: true,
-    message: "Hello",
+    message: "Hello from API Response!",
   });
 });
+console.log("Hello World!")
 const port = 3001;
 app.listen(port, () => {
   console.log(`The Port is running at ${port}`);
